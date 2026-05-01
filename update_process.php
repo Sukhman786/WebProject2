7d352997fdc9d4a9267646e31084ad9dfce3c21b
Sukhman786 <sukhman6921@gmail.com>
2026-05-01 14:50:19 +0530

    Code fixing & updating

--- update_process.php
+++ update_process.php
@@ -6,7 +6,6 @@ if (!$conn) {
     exit("Connection failed: " . $e['message']);
 }
 
-
 $id        = $_POST['raw_id'];
 $s_name    = $_POST['s_name'];
 $s_mobile  = $_POST['s_mobile'];
@@ -19,41 +18,51 @@ $weight    = $_POST['weight'];
 $d_type    = $_POST['d_type'];
 $p_mode    = $_POST['p_mode'];
 
+$get_ids_sql = "SELECT sender_id, receiver_id FROM parcelji WHERE RAW_ID = :rid";
+$get_stmt = oci_parse($conn, $get_ids_sql);
+oci_bind_by_name($get_stmt, ':rid', $id);
+oci_execute($get_stmt);
+$ids = oci_fetch_array($get_stmt, OCI_ASSOC);
 
-$sql = "UPDATE courier_bookings SET 
-            SENDER_NAME = :sn, 
-            SENDER_MOBILE = :sm, 
-            SENDER_ADDRESS = :sa, 
-            RECEIVER_NAME = :rn, 
-            RECEIVER_MOBILE = :rm, 
-            RECEIVER_ADDRESS = :ra, 
-            PARCEL_TYPE = :pt, 
-            WEIGHT_GRAMS = :wg, 
-            DELIVERY_TYPE = :dt, 
-            PAYMENT_MODE = :pm 
-        WHERE RAW_ID = :rid";
+if ($ids) {
+    $sid = $ids['SENDER_ID'];
+    $rid = $ids['RECEIVER_ID'];
 
-$stmt = oci_parse($conn, $sql);
+    $sql1 = "UPDATE senderji SET SENDER_NAME = :sn, SENDER_MOBILE = :sm, SENDER_ADDRESS = :sa WHERE sender_id = :sid";
+    $stmt1 = oci_parse($conn, $sql1);
+    oci_bind_by_name($stmt1, ':sn', $s_name);
+    oci_bind_by_name($stmt1, ':sm', $s_mobile);
+    oci_bind_by_name($stmt1, ':sa', $s_address);
+    oci_bind_by_name($stmt1, ':sid', $sid);
 
-oci_bind_by_name($stmt, ':sn', $s_name);
-oci_bind_by_name($stmt, ':sm', $s_mobile);
-oci_bind_by_name($stmt, ':sa', $s_address);
-oci_bind_by_name($stmt, ':rn', $r_name);
-oci_bind_by_name($stmt, ':rm', $r_mobile);
-oci_bind_by_name($stmt, ':ra', $r_address);
-oci_bind_by_name($stmt, ':pt', $p_type);
-oci_bind_by_name($stmt, ':wg', $weight);
-oci_bind_by_name($stmt, ':dt', $d_type);
-oci_bind_by_name($stmt, ':pm', $p_mode);
-oci_bind_by_name($stmt, ':rid', $id);
+    oci_execute($stmt1, OCI_NO_AUTO_COMMIT);
 
-if (oci_execute($stmt)) {
-    echo "Update Successful! The record has been synced with the database.";
-} else {
-    $e = oci_error($stmt);
-    echo "Database Error: " . $e['message'];
+    $sql2 = "UPDATE receiverji SET RECEIVER_NAME = :rn, RECEIVER_MOBILE = :rm, RECEIVER_ADDRESS = :ra WHERE receiver_id = :rid";
+    $stmt2 = oci_parse($conn, $sql2);
+    oci_bind_by_name($stmt2, ':rn', $r_name);
+    oci_bind_by_name($stmt2, ':rm', $r_mobile);
+    oci_bind_by_name($stmt2, ':ra', $r_address);
+    oci_bind_by_name($stmt2, ':rid', $rid);
+
+    oci_execute($stmt2, OCI_NO_AUTO_COMMIT);
+
+    $sql3 = "UPDATE parcelji SET PARCEL_TYPE = :pt, WEIGHT_GRAMS = :wg, DELIVERY_TYPE = :dt, PAYMENT_MODE = :pm WHERE RAW_ID = :rid";
+    $stmt3 = oci_parse($conn, $sql3);
+    oci_bind_by_name($stmt3, ':pt', $p_type);
+    oci_bind_by_name($stmt3, ':wg', $weight);
+    oci_bind_by_name($stmt3, ':dt', $d_type);
+    oci_bind_by_name($stmt3, ':pm', $p_mode);
+    oci_bind_by_name($stmt3, ':rid', $id);
+    
+    if (oci_execute($stmt3, OCI_NO_AUTO_COMMIT)) {
+        oci_commit($conn);
+        echo "Update Successful! The record has been synced with the database.";
+    } else {
+        oci_rollback($conn);
+        $e = oci_error($stmt3);
+        echo "Database Error: " . $e['message'];
+    }
 }
 
-oci_free_statement($stmt);
 oci_close($conn);
 ?>
